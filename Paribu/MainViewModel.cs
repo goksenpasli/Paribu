e9c2d2baf0c42944324aeee0ce8aa6b37562d55e
goksenpasli <gpasli@gmail.com>
2022-01-01 09:36:10 +0300

    Add project files.

--- Paribu/MainViewModel.cs
+++ Paribu/MainViewModel.cs
@@ -2,7 +2,6 @@
 using System;
 using System.ComponentModel;
 using System.Net;
-using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace Paribu
@@ -19,12 +18,16 @@ namespace Paribu
         public MainViewModel()
         {
             Veri = FromJson(reply);
-            İndir = new RelayCommand(_ => Download());
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = TimeSpan.FromSeconds(10) };
+            timer.Tick += (s, e) =>
+            {
+                reply = client.DownloadString(new Uri("https://www.paribu.com/ticker"));
+                Veri = FromJson(reply);
+            };
+            timer.Start();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public ICommand İndir { get; set; }
 
         public Data Veri { get; set; }
 
@@ -35,17 +38,6 @@ namespace Paribu
         private static Data FromJson(string json)
         {
             return JsonConvert.DeserializeObject<Data>(json, Converter.Settings);
-        }
-
-        private void Download()
-        {
-            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.SystemIdle) { Interval = TimeSpan.FromSeconds(15) };
-            timer.Start();
-            timer.Tick += (s, e) =>
-            {
-                reply = client.DownloadString(new Uri("https://www.paribu.com/ticker"));
-                Veri = FromJson(reply);
-            };
         }
     }
 }
