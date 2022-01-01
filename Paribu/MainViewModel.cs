a04bf9d5c6edc39f90fcb795b223a98f92730d94
goksenpasli <gpasli@gmail.com>
2022-01-01 09:19:50 +0300

    Add project files.

--- Paribu/MainViewModel.cs
+++ Paribu/MainViewModel.cs
@@ -1,24 +1,25 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.ComponentModel;
 using System.Net;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Paribu
 {
-
     public class MainViewModel : INotifyPropertyChanged
     {
         static MainViewModel()
         {
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072; //TLS 1.2
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
             client = new WebClient();
-            reply = client.DownloadString(new System.Uri("https://www.paribu.com/ticker"));
+            reply = client.DownloadString(new Uri("https://www.paribu.com/ticker"));
         }
 
         public MainViewModel()
         {
             Veri = FromJson(reply);
-            İndir = new RelayCommand(z => Download());
+            İndir = new RelayCommand(_ => Download());
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -27,19 +28,24 @@ namespace Paribu
 
         public Data Veri { get; set; }
 
-        public static Data FromJson(string json)
-        {
-            return JsonConvert.DeserializeObject<Data>(json, Converter.Settings);
-        }
-
         private static readonly WebClient client;
 
         private static string reply;
 
+        private static Data FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Data>(json, Converter.Settings);
+        }
+
         private void Download()
         {
-            reply = client.DownloadString(new System.Uri("https://www.paribu.com/ticker"));
-            Veri = FromJson(reply);
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.SystemIdle) { Interval = TimeSpan.FromSeconds(15) };
+            timer.Start();
+            timer.Tick += (s, e) =>
+            {
+                reply = client.DownloadString(new Uri("https://www.paribu.com/ticker"));
+                Veri = FromJson(reply);
+            };
         }
     }
 }
